ba383b749b2e6f9a75526d716e95014609fcd19c
oskarbay01 <aidosuly0@gmail.com>
2021-12-28 16:04:16 +0600

    "Add prepareStatement "

--- src/com/jdbc/starter/PrepareStatementRunner.java
+++ src/com/jdbc/starter/PrepareStatementRunner.java
@@ -3,6 +3,8 @@ package com.jdbc.starter;
 import com.jdbc.starter.util.ConnectionManager;
 
 import java.sql.*;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -12,6 +14,38 @@ public class PrepareStatementRunner {
 
         var result = getTicketByFlightId(flightId);
         System.out.println(result);
+
+        var result1 = getFlightsBetween(LocalDate.of(2020, 1, 1).atStartOfDay(), LocalDateTime.now());
+        System.out.println(result1);
+    }
+
+    private static List<Long> getFlightsBetween(LocalDateTime start, LocalDateTime end) throws SQLException {
+        String sql = """
+                SELECT id
+                FROM flight
+                WHERE departure_date BETWEEN ? AND ?
+                """;
+
+        List<Long> result = new ArrayList<>();
+        try (var connection = ConnectionManager.open();
+             var prepareStatement = connection.prepareStatement(sql)) {
+            System.out.println(prepareStatement);
+
+            // установили ? на prepareStatement
+            prepareStatement.setTimestamp(1, Timestamp.valueOf(start));
+            System.out.println(prepareStatement);
+            prepareStatement.setTimestamp(2, Timestamp.valueOf(end));
+            System.out.println(prepareStatement);
+
+            var resultSet = prepareStatement.executeQuery();
+            while (resultSet.next()) {
+                result.add(
+                        resultSet.getObject(1,Long.class)
+                );
+            }
+        }
+        return result;
+
     }
 
     private static List<Long> getTicketByFlightId(Long flight) throws SQLException {
